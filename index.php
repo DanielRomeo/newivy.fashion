747d9ed0fcdbba1b9046e2b2ec2565cff159f685
Daniel Mamphekgo <Danielromeo99@gmail.com>
2020-01-08 16:53:36 +0200

    make1

---
+++ index.php
@@ -0,0 +1,84 @@
+<!doctype html>
+<html lang="en">
+<head>
+<!-- Required meta tags -->
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+	<!-- Bootstrap CSS -->
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+	<link rel="stylesheet" type="text/css" href="index.css">
+	<title>Hello, world!</title>
+</head>
+<body>
+
+
+	<div class="container">
+
+
+		
+
+		<!-- --------------------------------------------------------- -->
+		<section>
+
+		  <div class="container">
+
+		    <div id="mainLoginBox" class="card">
+
+		      <div id="mainLoginBoxRow" class="row">
+
+
+		      	<div style=" height: 100%;" class="col-md-6">
+		         	<div id="imagePlacement" class="card-img-bottom"></div>
+		        </div>
+
+		        <!-- start of form -------------------------------------- -->
+
+		        <div class="col-md-6">
+
+		          <div style="margin-right: 10px;" class="card-block">
+
+		            <h1><i>N E W I V Y . F A S H I O N</i></h1>
+		           	<hr>
+		           	<h2>Login</h2>
+
+		           	<form>
+						<div class="form-group">
+							<label for="exampleInputEmail1">Email address</label>
+
+							<input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+
+							<small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+						</div>
+
+						<div class="form-group">
+							<label for="exampleInputPassword1">Password</label>
+							<input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+						</div>
+
+						<div class="form-group form-check">
+							<input type="checkbox" class="form-check-input" id="exampleCheck1">
+							<label class="form-check-label" for="exampleCheck1">&nbsp; Remember me</label>
+						</div>
+
+						<button type="submit" class="btn btn-primary">Login</button>
+					</form>
+
+		          </div>
+		        </div>
+		      </div>
+		    </div>
+		  </div>
+		</section>
+
+		<!-- --------------------------------------------------------- -->
+	</div>
+
+
+<!-- Optional JavaScript -->
+<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+</body>
+</html>
